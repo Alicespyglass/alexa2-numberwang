a4b437e91cc146ac9cdd39f7be099f8d5eae22f5
alicespyglass <alicespyglass@gmail.com>
2017-05-01 12:27:37 +0100

    bug fix in server.rb - test now works

--- numberwang_app/server.rb
+++ numberwang_app/server.rb
@@ -3,14 +3,14 @@ require 'json'
 require 'net/http'
 
 post '/' do
-  number_facts_uri = URI("http://numberspi.com/42")
+  number_facts_uri = URI("http://numbersapi.com/42")
   number_fact = Net::HTTP.get(number_facts_uri)
   {
     version: "1.0",
     response: {
       outputSpeech: {
         type: "PlainText",
-        text: "number_fact"
+        text: number_fact
       }
     }
   }.to_json

0838f9416317423a051956e85ef5633c0751c476
alicespyglass <alicespyglass@gmail.com>
2017-05-01 23:44:38 +0100

    Handling custom slot value in sinatra. Not currently passing in developer window - 404 error

--- numberwang_app/server.rb
+++ numberwang_app/server.rb
@@ -3,8 +3,13 @@ require 'json'
 require 'net/http'
 
 post '/' do
-  number = JSON.parse(request.body.read)["request"]["intent"]["slots"]["Number"]["value"]
-  number_facts_uri = URI("http://numbersapi.com/#{number}")
+  require 'pry'; binding.pry
+  parsed_request = JSON.parse(request.body.read)
+
+  number = parsed_request["request"]["intent"]["slots"]["Number"]["value"]
+  fact_type = parsed_request["request"]["intent"]["slots"]["FactType"]["value"]
+
+  number_facts_uri = URI("http://numbersapi.com/#{number}/#{fact_type}")
   number_fact = Net::HTTP.get(number_facts_uri)
   {
     version: "1.0",
